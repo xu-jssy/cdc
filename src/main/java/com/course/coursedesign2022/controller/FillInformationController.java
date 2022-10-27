4334880874d1aae93611ae9d7aafe71a7f08fb27
B19031630陶岳隆 <2293164297@qq.com>
2022-10-27 15:08:45 +0800

    填写个人信息功能实现

--- src/main/java/com/course/coursedesign2022/controller/FillInformationController.java
+++ src/main/java/com/course/coursedesign2022/controller/FillInformationController.java
@@ -1,4 +1,34 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.mapper.loginUserMapper;
+import com.course.coursedesign2022.pojo.loginUser;
+import com.course.coursedesign2022.service.FillInformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 public class FillInformationController {
+    @Autowired
+    public loginUserMapper loginUserMapper;
+
+    @Autowired
+    private FillInformation fillIn;
+
+
+    @PutMapping(value = "/loginUser/fillIn/{zhanghao}")//修改个人信息
+    public loginUser updateInformation(@PathVariable("zhanghao") Integer zhanghao,
+                                         @RequestParam("information") Integer information){
+        loginUser loginUser = loginUserMapper.selectByPrimaryKey(zhanghao);
+//        System.out.println(loginUser.getZhanghao());
+//        System.out.println(loginUser.getInformation());
+//        System.out.println(loginUserMapper.selectByPrimaryKey(zhanghao).getInformation());
+        fillIn.Fillinformation(loginUser);//首次加分策略
+        if(information==-1){loginUser.setInformation(0);}
+        else {loginUser.setInformation(information);}
+        //loginUser.setInformation(information);
+        loginUserMapper.updateByPrimaryKey(loginUser);
+        return loginUserMapper.selectByPrimaryKey(zhanghao);
+    }
+
 }
--- src/main/java/com/course/coursedesign2022/service/FillInformation.java
+++ src/main/java/com/course/coursedesign2022/service/FillInformation.java
@@ -1,4 +1,32 @@
 package com.course.coursedesign2022.service;
 
+import com.course.coursedesign2022.mapper.UserMapper;
+import com.course.coursedesign2022.mapper.loginUserMapper;
+import com.course.coursedesign2022.pojo.PointObject;
+import com.course.coursedesign2022.pojo.loginUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FillInformation {
+    @Autowired
+    public loginUserMapper loginUserMapper;
+    @Autowired
+    public UserMapper userMapper;
+
+    public void Fillinformation(loginUser loginUser){
+
+        PointObject pointObject = userMapper.getPointObjectByID(loginUser.getId());
+        System.out.println("信息已修改");
+        if(loginUser.getInformation()==-1){
+
+            int grow=pointObject.getGrowScore();
+            pointObject.setGrowScore(grow+2);
+            userMapper.updatePointObject(pointObject);
+            System.out.println("成长积分+2");
+
+        }
+
+
+    }
 }
