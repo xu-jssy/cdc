674d58eefec33a49a878fb5ea898bc7fc26b57a5
xu-jssy <72542511+xu-jssy@users.noreply.github.com>
2022-11-03 06:14:49 +0800

    门诊随访
    门诊随访

--- src/main/java/com/course/coursedesign2022/controller/FollowUpController.java
+++ src/main/java/com/course/coursedesign2022/controller/FollowUpController.java
@@ -1,4 +1,26 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.mapper.UserMapper;
+import com.course.coursedesign2022.pojo.PointObject;
+import com.course.coursedesign2022.service.FollowUp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FollowUpController {
+    @Autowired
+    private FollowUp followUp;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @PutMapping(value = "/FollowUp")
+    public PointObject Followup(@RequestParam("id") Integer id){
+        System.out.println(id);
+        PointObject pointObject = userMapper.getPointObjectByID(id);
+        followUp.FollowUp(id);
+        return pointObject;
+    }
 }
