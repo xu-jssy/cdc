30966665ba433b42cb5fbf0344d83ac2af12485d
xu-jssy <72542511+xu-jssy@users.noreply.github.com>
2022-11-03 06:14:49 +0800

    门诊随访
    门诊随访

--- src/main/java/com/course/coursedesign2022/controller/FollowUpController.java
+++ src/main/java/com/course/coursedesign2022/controller/FollowUpController.java
@@ -1,4 +1,22 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.mapper.UserMapper;
+import com.course.coursedesign2022.pojo.PointObject;
+import com.course.coursedesign2022.service.FollowUp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class FollowUpController {
+    @Autowired
+    private FollowUp followUp;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping(value="/user/FollowUp/{id}")
+    public PointObject followUp(@PathVariable("id") Integer id){
+        followUp.FollowUp(id);
+        return userMapper.getPointObjectByID(id);
+    }
 }
--- src/test/java/com/course/coursedesign2022/controller/restfulTest.java
+++ src/test/java/com/course/coursedesign2022/controller/restfulTest.java
@@ -26,34 +26,34 @@ public class restfulTest {
     @Autowired
     UserMapper userMapper;
 
-    public void printUser(int id){
-        PointObject pointObject=userMapper.getPointObjectByID(id);
+    public void printUser(int id) {
+        PointObject pointObject = userMapper.getPointObjectByID(id);
         System.out.println(pointObject);
     }
 
-    public void printAllUser(){
-        List<PointObject> pointObjects=userMapper.getPointObject();
-        for(PointObject pointObject:pointObjects){
+    public void printAllUser() {
+        List<PointObject> pointObjects = userMapper.getPointObject();
+        for (PointObject pointObject : pointObjects) {
             System.out.println(pointObject);
         }
         System.out.println("---------");
     }
 
     @Test
-    public void testGet() throws Exception{
+    public void testGet() throws Exception {
         printAllUser();
         RequestBuilder requestBuilder;
-        requestBuilder=get("/user/");
+        requestBuilder = get("/user/");
         mockMvc.perform(requestBuilder).
                 andExpect(status().isOk()).
                 andExpect(content().string(equalTo("[{\"id\":3,\"growScore\":3,\"exchangeScore\":3,\"scoreTotal\":5},{\"id\":4,\"growScore\":1,\"exchangeScore\":1,\"scoreTotal\":2}]")));
     }
 
     @Test
-    public void testAddUser() throws Exception{
+    public void testAddUser() throws Exception {
         printAllUser();
         RequestBuilder requestBuilder;
-        requestBuilder=post("/user/?id=5&exchange=0&grow=0&total=0");
+        requestBuilder = post("/user/?id=5&exchange=0&grow=0&total=0");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk()).andExpect(content().string(equalTo("[{\"id\":1,\"growScore\":0,\"exchangeScore\":0,\"scoreTotal\":0},{\"id\":3,\"growScore\":3,\"exchangeScore\":3,\"scoreTotal\":5},{\"id\":4,\"growScore\":0,\"exchangeScore\":0,\"scoreTotal\":0}]")));
         printAllUser();
@@ -61,10 +61,10 @@ public class restfulTest {
 
 
     @Test
-    public void testGetByID() throws Exception{
+    public void testGetByID() throws Exception {
         printUser(1);
         RequestBuilder requestBuilder;
-        requestBuilder=get("/user/query/1");
+        requestBuilder = get("/user/query/1");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("{\"id\":1,\"growScore\":0,\"exchangeScore\":0,\"scoreTotal\":0}")));
@@ -72,10 +72,10 @@ public class restfulTest {
 
 
     @Test
-    public void testDeleteUser() throws Exception{
+    public void testDeleteUser() throws Exception {
         printAllUser();
         RequestBuilder requestBuilder;
-        requestBuilder=delete("/user/1");
+        requestBuilder = delete("/user/1");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("[{\"id\":3,\"growScore\":1,\"exchangeScore\":1,\"scoreTotal\":1}]")));
@@ -83,11 +83,11 @@ public class restfulTest {
     }
 
     @Test
-    public void testUpdateUser() throws Exception{
+    public void testUpdateUser() throws Exception {
 
         printUser(1);
         RequestBuilder requestBuilder;
-        requestBuilder=put("/user/3?exchange=1&grow=1&total=1");
+        requestBuilder = put("/user/3?exchange=1&grow=1&total=1");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("{\"id\":1,\"growScore\":0,\"exchangeScore\":0,\"scoreTotal\":0}")));
@@ -96,13 +96,15 @@ public class restfulTest {
 
 
     @Test
-    public void  testBasic() throws Exception{
+    public void testBasic() throws Exception {
         printUser(1);
         RequestBuilder requestBuilder;
-        requestBuilder=put("/testDesign/1");
+        requestBuilder = put("/testDesign/1");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("ok")));
         printUser(1);
     }
+
+
 }
