cf6c707ec8d1ff021c7366c76b9b7812f2eade29
JuWuhe <1049966282@qq.com>
2022-11-03 22:29:44 +0800

    并发症

--- src/main/java/com/course/coursedesign2022/controller/BfzNoteController.java
+++ src/main/java/com/course/coursedesign2022/controller/BfzNoteController.java
@@ -1,4 +1,25 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.mapper.RecordTimeMapper;
+import com.course.coursedesign2022.pojo.RecordTime;
+import com.course.coursedesign2022.service.BfzNote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class BfzNoteController {
+    @Autowired
+    private RecordTimeMapper recordTimeMapper;
+
+    @Autowired
+    private BfzNote bfzNote;
+
+    @PutMapping(value="/BfzRecord")
+    public RecordTime FillBfzRecord(@RequestParam("id") Integer id){
+        RecordTime recordTime = recordTimeMapper.selectByPrimaryKey(id);
+        long now = bfzNote.BfzNote(recordTime);
+        recordTime.setBfztime(now);
+        recordTimeMapper.updateByPrimaryKey(recordTime);
+        return recordTimeMapper.selectByPrimaryKey(id);
+    }
 }
--- src/main/java/com/course/coursedesign2022/service/BfzNote.java
+++ src/main/java/com/course/coursedesign2022/service/BfzNote.java
@@ -1,4 +1,37 @@
 package com.course.coursedesign2022.service;
 
+import com.course.coursedesign2022.mapper.RecordTimeMapper;
+import com.course.coursedesign2022.mapper.UserMapper;
+import com.course.coursedesign2022.pojo.PointObject;
+import com.course.coursedesign2022.pojo.RecordTime;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 public class BfzNote {
+    @Autowired
+    private RecordTimeMapper recordTimeMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public long BfzNote(RecordTime recordTime){
+        PointObject pointObject = userMapper.getPointObjectByID(recordTime.getId());
+        System.out.println("并发症记录成功");
+
+        long start = LocalDateTime.now().withYear(LocalDateTime.now().getYear()).withMonth(1).withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).toEpochSecond(ZoneOffset.ofHours(8));
+        long end = LocalDateTime.now().withYear(LocalDateTime.now().getYear()).withMonth(12).withDayOfMonth(31).withHour(23).withMinute(59).withSecond(59).toEpochSecond(ZoneOffset.ofHours(8));
+        long now = LocalDateTime.now().toEpochSecond(ZoneOffset.ofHours(8));
+        long last = recordTime.getBfztime();
+
+        if(last<start || last>end){
+            int grow=pointObject.getGrowScore();
+            pointObject.setGrowScore(grow+3);
+            pointObject.setScoreTotal(pointObject.getGrowScore()+pointObject.getExchangeScore());
+            userMapper.updatePointObject(pointObject);
+            System.out.println("成长积分+3");
+        }
+        return now;
+    }
 }
