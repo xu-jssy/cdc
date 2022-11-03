2533f90ba96b23b32396caf624d1ad7a5a3b511b
JuWuhe <1049966282@qq.com>
2022-11-03 22:30:59 +0800

    评估报告

--- src/main/java/com/course/coursedesign2022/controller/EvaluateReportController.java
+++ src/main/java/com/course/coursedesign2022/controller/EvaluateReportController.java
@@ -1,4 +1,22 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.mapper.RecordTimeMapper;
+import com.course.coursedesign2022.pojo.RecordTime;
+import com.course.coursedesign2022.service.EvaluateReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class EvaluateReportController {
+    @Autowired
+    private RecordTimeMapper recordTimeMapper;
+
+    @Autowired
+    private EvaluateReport evaluateReport;
+
+    @PutMapping(value="/EvaluateReport")
+    public RecordTime CreateEvaluateReport(@RequestParam("id") Integer id){
+        evaluateReport.EvaluateReport(id);
+        return recordTimeMapper.selectByPrimaryKey(id);
+    }
 }
